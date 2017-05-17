370f87ee0b8441d36392de77125f7ea4d4b8e208
Harry Slatyer <harry.slatyer@gmail.com>
2017-05-17 16:24:09 +1000

    Make moving-computation-to-graph work
    It's not pretty, largely because it's with TF 0.something and doesn't
    have the newer (nicer) 1.0 API.
    
    But it works.
    
    Important thing is to create the training step inside the while loop,
    otherwise it just gets run once.

--- tf/test/graph.py
+++ tf/test/graph.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import time
 import json
 import math
 import matplotlib.pyplot as plt
@@ -17,9 +18,10 @@ INPUT_DIM = 1
 HIDDEN_LAYER_DIMS = [32] * 5
 ACTS = [tf.nn.relu] * 5
 OUTPUT_DIM = 1
+TRAIN_SIZE = 20
 
 # Training
-BATCH_SIZE = 100
+BATCH_SIZE = 10
 TRAIN_REG_CO = 0#.001
 TRAINER = tf.train.AdamOptimizer()
 INITIAL_STD = 0.1
@@ -30,27 +32,18 @@ class Net():
         self.session = tf.Session(graph=self.graph)
 
         with self.graph.as_default():
-            self.x = tf.placeholder(tf.float32, shape=[None, INPUT_DIM])
-            self.y_ = tf.placeholder(tf.float32, shape=[None, OUTPUT_DIM])
+            self.tx = tf.placeholder_with_default(tf.zeros([TRAIN_SIZE, INPUT_DIM]), shape=[TRAIN_SIZE,INPUT_DIM])
+            self.ty_ = tf.placeholder_with_default(tf.zeros([TRAIN_SIZE, OUTPUT_DIM]), shape=[TRAIN_SIZE,OUTPUT_DIM])
+            self.bx = tf.placeholder_with_default(tf.zeros([BATCH_SIZE, INPUT_DIM]), shape=[BATCH_SIZE,INPUT_DIM])
+            self.by_ = tf.placeholder_with_default(tf.zeros([BATCH_SIZE, OUTPUT_DIM]), shape=[BATCH_SIZE,OUTPUT_DIM])
+            self.x = tf.placeholder_with_default(tf.zeros([1, INPUT_DIM]), shape=[1,INPUT_DIM])
+            self.y_ = tf.placeholder_with_default(tf.zeros([1, OUTPUT_DIM]), shape=[1,OUTPUT_DIM])
             self.reg_co = tf.placeholder_with_default(0., shape=[])
+            self.epochs = tf.placeholder_with_default(1, shape=[])
 
             # Variables.
             Ws = []
             bs = []
-
-            nex = tf.shape(self.x)[0]
-            shape = [BATCH_SIZE]*int(nex/BATCH_SIZE)
-            if nex % BATCH_SIZE != 0:
-                shape += [nex % BATCH_SIZE]
-            xbatches = tf.split(self.x, shape)
-            ybatches = tf.split(self.y_, shape)
-            #X,Y = tf.train.shuffle_batch(
-            #        [self.x,self.y_],
-            #        batch_size=BATCH_SIZE,
-            #        capacity=1000,
-            #        min_after_dequeue=5,
-            #        enqueue_many=True,
-            #        allow_smaller_final_batch=True)
 
             prev_layer_dim = INPUT_DIM
             for dim in HIDDEN_LAYER_DIMS:
@@ -71,18 +64,43 @@ class Net():
                   prev_h = act(tf.matmul(prev_h, W) + b)
                 return tf.matmul(prev_h, Wout) + bout
 
-            self.y = get_y(X)
+            self.y = get_y(self.x)
 
             def get_loss(x, y):
                 return (
                     tf.reduce_mean(tf.reduce_sum(tf.square(get_y(x) - y), reduction_indices=[1]))
                     + self.reg_co * tf.reduce_mean([tf.nn.l2_loss(W) for W in Ws + [Wout]]))
 
+            self.loss_func = get_loss(self.tx, self.ty_)
+
             # Training.
-            self.loss_func = (
-                    tf.reduce_mean(tf.reduce_sum(tf.square(self.y - Y), reduction_indices=[1]))
-                    + self.reg_co * tf.reduce_mean([tf.nn.l2_loss(W) for W in Ws + [Wout]]))
-            self.train_step = TRAINER.minimize(self.loss_func)
+            def make_training_step():
+                print("Hello")
+                nexamples = TRAIN_SIZE#tf.shape(self.x)[0]
+                #shape = tf.tile([BATCH_SIZE], [tf.floordiv(nexamples, BATCH_SIZE)])
+                #remaining = tf.mod(nexamples, BATCH_SIZE)
+                #print(shape)
+                #print(remaining)
+                #print([remaining])
+                #shape = tf.cond(
+                #        tf.equal(remaining, 0),
+                #        lambda: shape,
+                #        lambda: tf.concat(0, [shape, [remaining]]))
+                xall = tf.split(0, nexamples, self.tx)
+                yall = tf.split(0, nexamples, self.ty_)
+                xbatches = [tf.concat(0, xall[i*BATCH_SIZE:(i+1)*BATCH_SIZE]) for i in range(int(nexamples/BATCH_SIZE))]
+                ybatches = [tf.concat(0, yall[i*BATCH_SIZE:(i+1)*BATCH_SIZE]) for i in range(int(nexamples/BATCH_SIZE))]
+                return tf.tuple([tf.zeros([1]), tf.zeros([1])], control_inputs=[TRAINER.minimize(get_loss(x, y)) for (x,y) in zip(xbatches,ybatches)])[0]
+            self.train_step = make_training_step()
+            #self.many_train_steps = tf.tile(self.train_step, [self.epochs])
+
+            i = tf.constant(0)
+            self.many_train_steps = tf.while_loop(
+                    lambda i: tf.less(i, self.epochs),
+                    lambda i: tf.tuple((tf.add(i,1),make_training_step()))[0],
+                    [i])
+
+            self.one_train_step = TRAINER.minimize(get_loss(self.bx, self.by_))
 
             # Gradient with respect to x.
             self.dydx = tf.gradients(self.y, self.x)[0]
@@ -98,25 +116,25 @@ class Net():
 
     def loss(self, xs, ys, reg=True):
         return self.session.run(self.loss_func, feed_dict={
-            self.x: xs,
-            self.y_: ys,
+            self.tx: xs,
+            self.ty_: ys,
             self.reg_co: TRAIN_REG_CO if reg else 0,
             })
 
     def train_once(self):
         self.session.run(self.train_step, feed_dict={
-            self.x: train_x,
-            self.y_: train_y,
+            self.tx: train_x,
+            self.ty_: train_y,
             self.reg_co: TRAIN_REG_CO,
             })
-        #all_indices = np.random.permutation(len(train_x))
+        #all_indices = range(len(train_x))#np.random.permutation(len(train_x))
         #for j in range(math.ceil(len(all_indices) / BATCH_SIZE)):
         #    batch_indices = all_indices[j * BATCH_SIZE : (j + 1) * BATCH_SIZE]
         #    batch_x = [train_x[index] for index in batch_indices]
         #    batch_y = [train_y[index] for index in batch_indices]
-        #    self.session.run(self.train_step, feed_dict={
-        #        self.x: batch_x,
-        #        self.y_: batch_y,
+        #    self.session.run(self.one_train_step, feed_dict={
+        #        self.bx: batch_x,
+        #        self.by_: batch_y,
         #        self.reg_co: TRAIN_REG_CO,
         #        })
         this_loss = self.loss(train_x, train_y)
@@ -132,11 +150,18 @@ class Net():
     def train(self, epochs=None, plot=False):
         losses = []
         try:
-            counter = 0
-            while epochs == None or counter < epochs:
-                this_loss = self.train_once()
-                losses.append(math.log(1+this_loss))
-                counter += 1
+            print(self.session.run(self.many_train_steps, feed_dict={
+                self.tx: train_x,
+                self.ty_: train_y,
+                self.reg_co: TRAIN_REG_CO,
+                self.epochs: epochs,
+                }))
+
+            #counter = 0
+            #while epochs == None or counter < epochs:
+            #    this_loss = self.train_once()
+            #    #losses.append(math.log(1+this_loss))
+            #    counter += 1
         except KeyboardInterrupt:
             pass
 
@@ -216,7 +241,10 @@ def an():
     nets.append(Net())
 
 reset()
-for _ in range(20):
+for _ in range(TRAIN_SIZE):
     explore_random()
 nets.append(Net())
-nets[0].train(10)
+start = time.time()
+nets[0].train(100)
+print("Time: " + str(time.time() - start))
+print("Loss: " + str(nets[0].loss(train_x,train_y)))
