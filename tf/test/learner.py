5e611e0b036e187d35a942e0fc71f30eb8e362fd
Harry Slatyer <harry.slatyer@gmail.com>
2017-05-21 12:30:57 +1000

    Use log of cost when training
    This causes more emphasis to be given to lower costs.

--- tf/test/learner.py
+++ tf/test/learner.py
@@ -10,17 +10,17 @@ def gelu_fast(_x):
     return 0.5 * _x * (1 + tf.tanh(tf.sqrt(2 / np.pi) * (_x + 0.044715 * tf.pow(_x, 3))))
 
 def eval_y(x):
-    return abs(x)
+    return x**2
 
 # Network architecture
 INPUT_DIM = 1
 HIDDEN_LAYER_DIMS = [32] * 5
-ACTS = [gelu_fast] * 5
+ACTS = [tf.nn.relu] * 5
 OUTPUT_DIM = 1
 
 # Training
 BATCH_SIZE = 100
-TRAIN_KEEP_PROB = 0.8
+TRAIN_KEEP_PROB = 1#0.8
 TRAIN_REG_CO = 0#.001
 TRAINER = tf.train.AdamOptimizer()
 INITIAL_STD = 0.1
@@ -103,7 +103,7 @@ def train_once():
     for j in range(math.ceil(len(all_indices) / BATCH_SIZE)):
         batch_indices = all_indices[j * BATCH_SIZE : (j + 1) * BATCH_SIZE]
         batch_x = [train_x[index] for index in batch_indices]
-        batch_y = [train_y[index] for index in batch_indices]
+        batch_y = np.log([train_y[index] for index in batch_indices])
         session.run(train_step, feed_dict={x: batch_x, y_: batch_y, keep_prob: TRAIN_KEEP_PROB,
             reg_co: TRAIN_REG_CO,
             y_ran: get_ran(),
@@ -136,7 +136,8 @@ def optimise(plot=False):
             fun=fun,
             x0=[np.random.uniform(-1,1)],
             bounds=[(-2,2)],
-            jac=grad).x
+            jac=grad,
+            tol=1e-8).x
     #xopt = opt.basinhopping(
     #        func=fun,
     #        x0=[0]).x
@@ -149,12 +150,12 @@ def _get_xrange():
     _wid_train_x = max(train_x)[0] - min(train_x)[0]
     min_plot_x = _mid_train_x - _wid_train_x * 0.75
     max_plot_x = _mid_train_x + _wid_train_x * 0.75
-    return [[x] for x in np.linspace(min_plot_x, max_plot_x, 1000)]
+    return [[x] for x in np.linspace(min_plot_x, max_plot_x, 10000)]
 
 def _get_ys_dist(xs):
     repeated_xs = xs * LCB_REPS
     print("Getting distribution results")
-    repeated_ys = session.run(y, feed_dict={x: repeated_xs, keep_prob: TRAIN_KEEP_PROB})
+    repeated_ys = np.exp(session.run(y, feed_dict={x: repeated_xs, keep_prob: TRAIN_KEEP_PROB}))
     print("Got distribution results")
     ys = [repeated_ys[i::len(xs)] for i in range(len(xs))]
     ys_actual = [[a[0] for a in y] for y in ys]
@@ -167,7 +168,7 @@ def _get_ys_dist(xs):
 def _get_ys_lcb(xs):
     repeated_xs = xs * LCB_REPS
     print("Getting distribution results")
-    repeated_ys = session.run(y, feed_dict={x: repeated_xs, keep_prob: TRAIN_KEEP_PROB})
+    repeated_ys = np.exp(session.run(y, feed_dict={x: repeated_xs, keep_prob: TRAIN_KEEP_PROB}))
     print("Got distribution results")
     ys = [repeated_ys[i::len(xs)] for i in range(len(xs))]
     ys_actual = [[a[0] for a in y] for y in ys]
@@ -180,7 +181,7 @@ def plot():
     predicted_x = _get_xrange()
     #ydist = _get_ys_dist(predicted_x)
     ylcb = _get_ys_lcb(predicted_x)
-    predicted_y = [r[0] for r in session.run(y, feed_dict={x: predicted_x})]
+    predicted_y = np.exp([r[0] for r in session.run(y, feed_dict={x: predicted_x})])
     plt.clf()
     #for (i,ys) in enumerate(ydist):
     #    plt.scatter(predicted_x, ys, c=[i]*len(ys), vmin=0, vmax=len(ydist), cmap=plt.get_cmap("viridis"))
